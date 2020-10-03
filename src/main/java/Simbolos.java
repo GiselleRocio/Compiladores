fcaa67a08e9ee92f49af84d4015f0707b010f34d
Giselle <gisecavs@gmail.com>
2020-10-03 20:20:49 -0300

    archivo Simbolos

---
+++ src/main/java/Simbolos.java
@@ -0,0 +1,40 @@
+public class Simbolos {
+    public static final int EOF = 0;
+    public static final int ERROR = 1;
+    public static final int PUT = 2;
+    public static final int GET = 3;
+    public static final int DIM = 4;
+    public static final int AS = 5;
+    public static final int INTEGER = 6;
+    public static final int FLOAT = 7;
+    public static final int WHILE = 8;
+    public static final int IF = 9;
+    public static final int ELSE = 10;
+    public static final int OP_AS = 11;
+    public static final int OP_SUM = 12;
+    public static final int OP_RES = 13;
+    public static final int OP_DIV = 14;
+    public static final int OP_MUL = 15;
+    public static final int OP_MEN = 16;
+    public static final int OP_MENIG = 17;
+    public static final int OP_MAY = 18;
+    public static final int OP_MAYIG = 19;
+    public static final int OP_DIST = 20;
+    public static final int OP_COMP = 21;
+    public static final int COMA = 22;
+    public static final int PYC = 23;
+    public static final int PA = 24;
+    public static final int PC = 25;
+    public static final int LA = 26;
+    public static final int LC = 27;
+    public static final int CA = 28;
+    public static final int CC = 29;
+    public static final int CTE_HEXA = 30;
+    public static final int CTE_BIN = 31;
+    public static final int CONST_INT = 32;
+    public static final int CONST_STR = 33;
+    public static final int CONST_FLOAT = 34;
+    public static final int ID = 35;
+
+
+}
